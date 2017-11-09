2cb94dd5e5cf3bee13cb59c3f71275fd9a53fb0f
Jesse Arellano <jessearellano10@gmail.com>
2017-11-09 12:35:57 -0800

    Initial Commit

---
+++ MasterMindGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MasterMindGame
+//
+//  Created by Jesse Arellano on 11/9/17.
+//  Copyright © 2017 Jesse Arellano. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
